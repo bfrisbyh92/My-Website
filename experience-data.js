d1665ae3cad1d05f9c93f3d6e742695a97cb9cdb
bfrisbyh92 <bfrisbyh92@gmail.com>
2023-02-01 17:29:29 -0500

    Changed text again.

--- experience-data.js
+++ experience-data.js
@@ -4,7 +4,7 @@ var experienceData = [
     date: "Jan 18th, 2022 - Current",
     organization: "Freelance Web-Mobile App Development",
     position: "Fiverr & Upwork",
-    desc: `Offering services in software engineering, web development, penetration testing, and security analysis for mobile and web applications. Available for freelance projects and active on multiple platforms(Fiverr, Upwork, Freelancer). I also take projects on from word of mouth, family, friends, and just general networking. From Landing Pages, Mobile Apps, Dashboards, Automation scripts, Logo Design, and so much more. These are just some of the listings I have up and take regularly.`,
+    desc: `Offering services in software engineering, web development, penetration testing, and security analysis for mobile and web applications. Active on multiple  platforms(Fiverr, Upwork, Freelancer).`,
     website: 'https://brendanfrisby.vercel.app'
   },
   {
@@ -12,7 +12,7 @@ var experienceData = [
     date: "2019 - 2022",
     organization: "Sovanah Global Logistics",
     position: "Warehouse Associate",
-    desc: `At Sovanah Global Logistics, I worked in their warehouse. Since departing full time work, I still occasionally do work when they need hands. The people there have helped me become who I am today, and will continue to shape me in the future from their impact.`,
+    desc: `At Sovanah Global Logistics, I worked in their warehouse. Doing inventory and managing freight through Magaya Software. We built pallets and crates on occasion. I've also done some related work driving a box truck, not directly for Sovanah but derived from them.`,
     website: 'https://app.social3.club'
   },
   {
